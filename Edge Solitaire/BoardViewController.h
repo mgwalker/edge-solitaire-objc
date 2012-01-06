a8af6af9814030f447431cc0665413d387eedfe8
darkcooger <cooger@darkcooger.net>
2012-01-05 22:25:46 -0600

    • Board view (iPhone) and view controller

---
+++ Edge Solitaire/BoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  BoardViewController.h
+//  Edge Solitaire
+//
+//  Created by Greg Walker on 1/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIUniversalViewController.h"
+
+@interface BoardViewController : UIUniversalViewController
+
+@end
