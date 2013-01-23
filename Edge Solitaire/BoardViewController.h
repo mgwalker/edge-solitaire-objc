163f1dabac83a2fd4fbeff8e57783fd6f9bb20bc
darkcooger <cooger@darkcooger.net>
2013-01-22 18:38:51 -0600

    Remove references to UIUniversalVewController.h

--- Edge Solitaire/BoardViewController.h
+++ Edge Solitaire/BoardViewController.h
@@ -6,7 +6,6 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import "UIUniversalViewController.h"
 #import "CardSpot.h"
 #import <AVFoundation/AVFoundation.h>
 
--- Edge Solitaire/LandingViewController.h
+++ Edge Solitaire/LandingViewController.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import "UIUniversalViewController.h"
-
 @interface LandingViewController : UIViewController
 {
 	
